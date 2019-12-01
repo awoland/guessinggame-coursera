bc84c4ed060ad2b7f0fcd6b7cc610803e4fe7173
awoland <awoland@me.com>
2019-12-01 08:06:31 +0400

    this *first initial* commit to this repo

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+echo "Welcome to Coursera.org Unix Workbench game!"
+
+answer=0
+numfiles=$(ls | wc -w)
+while [[ $answer -ne $numfiles ]]
+do
+	echo -n "Plaease answer, how many files are in the current directory ? :"
+	read answer
+	if [[ $answer -eq $numfiles ]]
+	then
+		echo "Congrats to You! Your guess has been correct!"
+		continue
+	elif [[ $answer -lt $numfiles ]]
+	then
+		echo "Sorry, but number in Your guesse is too low. Please try guess it again."
+	else
+		echo "Sorry, but number in Your guesse is too high. Please try guess it again."
+	fi
+done
